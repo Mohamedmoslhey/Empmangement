2a80e061a2ce36ab7ecdd58a1ac728667c265982
Mohamed <Mohamed@DESKTOP-T6OHLBF>
2022-04-20 16:18:01 +0200

    20-04-2022

--- src/main/java/com/nsag/springbranch/Empmangement/rest/RestEmpController.java
+++ src/main/java/com/nsag/springbranch/Empmangement/rest/RestEmpController.java
@@ -3,6 +3,7 @@ package com.nsag.springbranch.Empmangement.rest;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,36 +17,37 @@ import com.nsag.springbranch.Empmangement.entity.Employee;
 import com.nsag.springbranch.Empmangement.service.EmployeeService;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/api")
 public class RestEmpController {
 
 	@Autowired
 	private EmployeeService empService;
 	
-	@GetMapping("/Employee")
+	@GetMapping("/employees")
 	public List<Employee> getList(){
 		
 		return empService.listofEmployee();
 	}
 	
-	@PostMapping("/Employee")
+	@PostMapping("/employees")
 	public Employee creatEmployeeProfile(@RequestBody Employee employee){
 		employee.setId(0);
 		return empService.updaCreate(employee);
 	}
-	@PutMapping("/Employee")
+	@PutMapping("/employees")
 	public Employee updEmployeeProfile(@RequestBody Employee employee){
 		
 		return empService.updaCreate(employee);
 	}
 	
-	@GetMapping("/Employee/{theEmplId}")
+	@GetMapping("/employees/{theEmplId}")
 	public Employee getList(@PathVariable int theEmplId){
 		
 		return empService.getEmployee(theEmplId);
 	}
 	
-	@DeleteMapping("/Employee/{theEmplId}")
+	@DeleteMapping("/employees/{theEmplId}")
 	public void delEmpl(@PathVariable int theEmplId){
 		
 		empService.deleteEmployee(theEmplId);
